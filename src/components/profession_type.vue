d7d15b5eb83c793b9098a2d8cbb3b02a1a0a3fc4
Ljj <724455927@qq.com>
2020-08-28 15:29:54 +0800

    4567

--- src/components/profession_type.vue
+++ src/components/profession_type.vue
@@ -3,7 +3,7 @@
     <h1>员工管理  <el-button type="success" @click="showDialogadd">添加</el-button></h1>-->
     <!-- data:绑定数据  height:声明之后会固定表头-->
     <el-button round @click="showDialog2()">添加</el-button>
-    <el-table :data="this.$route.query.profession_type" :stripe="true" border>
+    <el-table :data="this.$route.query.profession_type" :stripe="true" border >
       <!-- prop显示绑定的数据的属性 -->
       <el-table-column prop="ptid" label="编号"></el-table-column>
       <el-table-column prop="ptname" label="职业名称"></el-table-column>
@@ -16,7 +16,7 @@
     </el-table>
 
     <el-dialog width="40%" title="添加类型" :visible="addVisible">
-      <el-form label-width="100px" label-suffix="：" class="form"  ref="fm">
+      <el-form label-width="100px" label-suffix="：" :model="profession_type" class="form"  ref="fm" :rules="rules">
         <el-form-item label="职业名称" prop="ptname">
           <el-input v-model="profession_type.ptname" name="ptname"></el-input>
         </el-form-item>
@@ -28,7 +28,7 @@
     </el-dialog>
 
     <el-dialog width="40%" title="修改类型" :visible="updateVisible">
-      <el-form label-width="100px" label-suffix="：" class="form"  ref="fm">
+      <el-form label-width="100px" label-suffix="：" :model="profession_type" class="form"  ref="fm" :rules="rules">
         <el-form-item label="职业名称" prop="ptname">
           <el-input v-model="profession_type.ptname" name="ptname"></el-input>
         </el-form-item>
@@ -48,7 +48,24 @@ export default {
     return {
       updateVisible: false,
       addVisible: false,
-      profession_type: {}
+      profession_type: {},
+      rules: {
+        ptname: [
+          // require:进行校验,默认校验非空  message:提示信息  trigger:触发校验的事件
+          {required: true, message: '职业类型不能为空', trigger: 'blur'},
+          // 自定义校验规则
+          {
+            trigger: ['blur'],
+            validator: function (rule, value, callback) {
+              if (value.indexOf('_') == -1) {
+                callback()
+              } else {
+                callback(new Error('职业类型不能包含_特殊字符'))
+              }
+            }
+          }
+        ]
+      }
     }
   },
   methods: {
@@ -73,24 +90,34 @@ export default {
       this.profession_type = {}
     },
     update: function () {
-      this.$axios.post('http://localhost:8888/sweet/profession_type/update', this.$qs.stringify(this.profession_type))
-        .then(response => {
-          if (response.data = 1) {
-            alert('修改成功')
-          } else {
-            alert('修改失败')
-          }
-        })
+      this.$refs['fm'].validate(valid => {
+        alert(valid)
+        if (valid==true) {
+          this.$axios.post('http://localhost:8888/sweet/profession_type/update', this.$qs.stringify(this.profession_type))
+            .then(response => {
+              if (response.data = 1) {
+                alert('修改成功')
+              }
+            })
+        } else {
+          alert('修改失败')
+        }
+      })
     },
     add: function () {
-      this.$axios.post('http://localhost:8888/sweet/profession_type/add', this.$qs.stringify(this.profession_type))
-        .then(response => {
-          if (response.data = 1) {
-            alert('添加成功')
-          } else {
-            alert('添加失败')
-          }
-        })
+      this.$refs['fm'].validate(valid => {
+        if (valid==true) {
+          this.$axios.post('http://localhost:8888/sweet/profession_type/add', this.$qs.stringify(this.profession_type))
+            .then(response => {
+              if (response.data = 1) {
+                alert('添加成功')
+              }
+            })
+        } else {
+          this.addVisible = true
+          alert('添加失败')
+        }
+      })
     }
   }
 }
